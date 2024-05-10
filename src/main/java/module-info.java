d1bf13780249bb9bcbdec38f5c76ab815ca89d55
ZORD PC <s202270440@kfupm.edu.sa>
2024-05-10 04:10:37 +0300

    I continued on ui , add method to integrate between two fxml; changed the names of the files and icons.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sweproject12 {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.desktop;
 
     opens com.example.sweproject12 to javafx.fxml;
     exports com.example.sweproject12;
