162dae4594eb678face48b7a8996114ec3344ea4
ZORD PC <s202270440@kfupm.edu.sa>
2024-05-11 01:25:16 +0300

    try12

--- src/main/java/com/example/sweproject12/Controller.java
+++ src/main/java/com/example/sweproject12/Controller.java
@@ -67,7 +67,7 @@ public class Controller {
 
 
         }
-        //hi 12
+        //hi 123
     void changeScene(ActionEvent event, String fxmlFile,String title,String name, String password){
         Parent pane = null;
         if(name!= null && password != null){
